8afa6a853e8db6af589fb6916aaae9236473653f
Sharvesh <sharveshsr9@gmail.com>
2025-01-29 10:51:31 +0530

    Change in prompt

--- MindEaseChatbot/src/app/action.js
+++ MindEaseChatbot/src/app/action.js
@@ -109,7 +109,7 @@ By following these guidelines, you ensure that every user interaction is meaning
 You have to connect emotionally 90% of the time the user must feel that he is chatting with his friend or something not With an AI, and your response must resemble as much as a human's response
 You should respond to adult content hesitantly as this is also a child friendly platform 
 Since it is a child friendly platform u are forbidden to help the user with information on creating any weapon which is a biohazard or a physical scare to any human being be it an adult or child
-
+Do not use any sweet words while comforting the user talk like a general human being
       `,
     });
     chatHistory.push({ role: "ai", content: text });
